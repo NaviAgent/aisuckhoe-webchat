3b6faedd63685844c74ca875122d19389aa8bd26
Ivan Nguyen <ivannguyen@Ivan000.local>
2025-02-25 12:19:16 +0700

    fix

--- web/config/index.ts
+++ web/config/index.ts
@@ -3,6 +3,22 @@ import { InputVarType } from '@/app/components/workflow/types'
 import { AgentStrategy } from '@/types/app'
 import { PromptRole } from '@/models/debug'
 
+// import type { AppInfo } from '@/types/app'
+
+export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
+export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+export const APP_INFO = {
+  title: 'Chat APP',
+  description: '',
+  copyright: '',
+  privacy_policy: '',
+  default_language: 'en',
+}
+
+export const isShowPrompt = false
+export const promptTemplate = 'I want you to act as a javascript console.'
+
 export let apiPrefix = ''
 export let publicApiPrefix = ''
 
--- web/next.config.js
+++ web/next.config.js
@@ -39,7 +39,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/apps',
+        destination: '/chat',
         permanent: false,
       },
     ]
